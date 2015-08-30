f26aed042ad849ab02692cb32c01faa88239ebfb
Danielle H <daheber@gmail.com>
2015-08-30 12:07:15 -0700

    Version 1.0 files

---
+++ app.js
@@ -0,0 +1,108 @@
+//Get user's answer and check to make sure it's valid
+function getUserAnswer (choice) {
+    
+    playerChoice.innerHTML = 'Player: ' + choice;
+
+    //Disable user choice buttons
+    document.getElementById('rock').disabled = true;
+    document.getElementById('paper').disabled = true;
+    document.getElementById('scissors').disabled = true;
+
+     //Enable go button
+     goButton.disabled = false;
+
+     //Make go button visible
+     goButton.style.visibility = "visible";
+
+     userAnswer = choice;
+     console.log(userAnswer);
+     return userAnswer;
+ }
+
+ //Compare computer's answer to the user's answer to see who won
+function compare (){
+
+    //generate computer answer
+    compAnswer = Math.random();
+
+    //Convert computer's answer into rock, paper, or scissors
+    console.log('Computer: ' + compAnswer);
+
+    if(compAnswer < 0.33){
+        compAnswer = 'rock';
+    }
+    else if(compAnswer < 0.66){
+        compAnswer = 'paper';
+    }
+    else {
+        compAnswer = 'scissors';
+    }
+
+    computerChoice.innerHTML = 'Computer: ' + compAnswer;
+    
+    //compare the user answer to the computer answer
+    if (userAnswer === compAnswer){
+        result.innerHTML = "The result is a tie!";
+        tie = true;
+        goButton.disabled = true;
+        playAgainButton.style.visibility = "visible";
+        return tie;
+    }
+    
+    else if(userAnswer === "rock") {
+
+        if(compAnswer === "scissors") {
+            result.innerHTML = "rock wins";
+            tie = false;
+            goButton.disabled = true;
+            playAgainButton.style.visibility = "visible";
+            return tie;
+        }
+        else {
+            result.innerHTML = "paper wins";
+            tie= false;
+            goButton.disabled = true;
+            playAgainButton.style.visibility = "visible";
+            return tie;
+        }
+    }   
+    else if(userAnswer === "paper"){
+    
+        if(compAnswer === "rock"){
+            result.innerHTML = "paper wins";
+            tie=false;
+            goButton.disabled = true;
+            playAgainButton.style.visibility = "visible";
+            return tie;
+        }
+        else{
+            result.innerHTML = "scissors wins";
+            tie=false;
+            goButton.disabled = true;
+            playAgainButton.style.visibility = "visible";
+            return tie;
+        }
+    }
+    else if (userAnswer === "scissors"){
+        if(compAnswer === "rock"){
+            result.innerHTML = "rock wins";
+            tie=false;
+            goButton.disabled = true;
+            playAgainButton.style.visibility = "visible";
+            return tie;
+        }
+        else{
+            result.innerHTML = "scissors wins";
+            tie=false;
+            goButton.disabled = true;
+            playAgainButton.style.visibility = "visible";
+            return tie;
+        }
+    }
+}
+
+//refresh the page
+function refresh(){
+    location.reload();
+}
+
---
+++ readme.txt
@@ -0,0 +1,5 @@
+INSTALLATION INSTRUCTIONS:
+1) Open Chrome and type “chrome://extensions” into the address bar
+2) Check the “Developer mode” checkbox at the top of the page
+3) Click “Load unpacked extension…” and select the “RPSExtension” folder (the root directory from this .zip file)
+4) Make sure “Rock Paper Scissors” is Enabled.
